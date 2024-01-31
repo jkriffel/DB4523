552e53539a962b5929d1da18acf474350675f7e6
Zeanderson <zackanderson14@gmail.com>
2024-01-31 12:48:38 -0600

    Kirby is retarded so I fixed it

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # DB4523
 Repository for Database 4523 by James Riffel and Zachary Anderson
+
+# h1
+*Cool stuff
--- python/python/converter.py
+++
@@ -1,4 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv("input.txt",sep='\s+',header = None)
-df.to_csv('input.csv',header=None)
